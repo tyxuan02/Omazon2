990a692189c8738593cb3aa61a5b6c83d0fc83f5
Yu Xuan <u2102858@siswa.um.edu.my>
2021-11-29 11:38:35 +0800

    -link database to our project -user can create an account and store in database -use (email=test, password=test) to try to log in

--- src/main/java/com/example/omazonproject/HelloController.java
+++ src/main/java/com/example/omazonproject/HelloController.java
@@ -5,16 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -50,8 +51,24 @@ public class HelloController {
     private TextField username_SignUp;
 
     @FXML
+    // login message
+    private Label loginMessageLabel;
+
+    @FXML
+    // check password and confirm password
+    private Label confirmPasswordLabel;
+
+    @FXML
         // Sign-up button pressed at the sign-up page
     void signUpButtonPressed(MouseEvent event) {
+
+        if (passwordEntered_SignUp.getText().equals(confirmPassword_SignUp.getText()) && ((passwordEntered_SignUp.getText() != "") && (confirmPassword_SignUp.getText() != ""))) {
+            registerUser();
+        } else {
+            confirmPasswordLabel.setText("Password does not match.");
+        }
+    }
+/*
         // Validating the email address entered by the user
         if (valEmail(emailEntered_SignUp.getText())) {
             // if valid email address is entered
@@ -71,12 +88,93 @@ public class HelloController {
             alert.showAndWait();
         }
     }
+    */
+
+
+
+    void registerUser(){
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String username = username_SignUp.getText();
+        String email = emailEntered_SignUp.getText();
+        String password = passwordEntered_SignUp.getText();
+        String insertFields = "INSERT INTO user_account (username, email, password) VALUES ('";
+        String insertValues = username + "','" + email + "','" + password +"')";
+        String insertToRegister = insertFields + insertValues;
+
+        try {
+            Statement statement = connectDB.createStatement();
+
+            if (!username_SignUp.getText().equals("") && !emailEntered_SignUp.getText().equals("") ) {
+
+                confirmPasswordLabel.setText("");
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Sign up successful.");
+                alert.setHeaderText(null);
+                alert.setContentText("Your account has been created.");
+                alert.showAndWait();
+                statement.executeUpdate(insertToRegister);
+
+            }
+            else {
+                confirmPasswordLabel.setText("");
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle(null);
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter data in correct format.");
+                alert.showAndWait();
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+            e.getCause();
+        }
+
+    }
+
 
     @FXML
         // Login button pressed
     void loginButtonPressed(MouseEvent event) {
-
+        if (!emailEntered_Login.getText().isBlank() && !passwordEntered_Login.getText().isBlank()){
+            loginMessageLabel.setText("");
+            validateLogin();
+        } else {
+            loginMessageLabel.setText("Please enter email and password");
+        }
     }
+
+    void validateLogin() {
+
+        try {
+            DatabaseConnection connectNow = new DatabaseConnection();
+            Connection connectDB = connectNow.getConnection();
+            Statement statement = connectDB.createStatement();
+            String email = emailEntered_Login.getText();
+            String password = passwordEntered_Login.getText();
+            ResultSet queryResult = statement.executeQuery("SELECT * FROM user_account WHERE email = '" + email + "' AND password ='" + password + "'");
+                if (queryResult.next()) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Login Successful!");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Welcome to Omazon");
+                    alert.showAndWait();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Invalid credentials. Please re-enter a valid email address.");
+                    alert.showAndWait();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+
+
+
 
     @FXML
         // Sign-up button pressed at the login page
