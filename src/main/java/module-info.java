280eb8819ee54cd54d0f4205a973bcf148802108
CHOO XIANG LUN <xlchoo25@gmail.com>
2021-12-20 18:08:43 +0800

    Fix module-info.java issue

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,8 @@ module com.example.omazonproject {
     requires java.sql;
     requires json.simple;
     requires java.mail;
-    requires activation;
     requires commons.io;
+    requires activation;
 
     opens com.example.omazonproject to javafx.fxml;
     exports com.example.omazonproject;
