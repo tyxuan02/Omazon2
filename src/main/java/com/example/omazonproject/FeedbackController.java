59506d7c1d0b7e73dbe16452c1d95184a06e1c09
Yu Xuan <u2102858@siswa.um.edu.my>
2022-01-12 13:45:18 +0800

    Update number of stars in database after user reviews product

--- src/main/java/com/example/omazonproject/FeedbackController.java
+++ src/main/java/com/example/omazonproject/FeedbackController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import org.controlsfx.control.Rating;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * This class is used as a controller for the feedback-page.fxml
  */
@@ -24,6 +28,51 @@ public class FeedbackController {
     void OKBtnPressed(ActionEvent event) {
         // update the rating in the database (ie. numberOfFiveStars++) and the json file
         int numberOfStars = (int) rating.getRating();
+
+        Connection connectDB = null;
+        Statement statement = null;
+
+        try {
+            DatabaseConnection connectNow = new DatabaseConnection();
+            connectDB = connectNow.getConnection();
+
+            String updateNumOfStars;
+
+            // insert number of stars into database
+            if (numberOfStars == 1) {
+                updateNumOfStars = "UPDATE product_info set numOfOneStars = IFNULL(numOfOneStars, 0) + 1 WHERE imageName = '" + imageName + "'";
+            } else if  (numberOfStars == 2) {
+                updateNumOfStars = "UPDATE product_info set numOfTwoStars = IFNULL(numOfTwoStars, 0) + 1 WHERE imageName = '" + imageName + "'";
+            } else if  (numberOfStars == 3) {
+                updateNumOfStars = "UPDATE product_info set numOfThreeStars = IFNULL(numOfThreeStars, 0) + 1 WHERE imageName = '" + imageName + "'";
+            } else if  (numberOfStars == 4) {
+                updateNumOfStars = "UPDATE product_info set numOfFourStars = IFNULL(numOfFourStars, 0) + 1 WHERE imageName = '" + imageName + "'";
+            } else {
+                updateNumOfStars = "UPDATE product_info set numOfFiveStars = IFNULL(numOfFiveStars, 0) + 1 WHERE imageName = '" + imageName + "'";
+            }
+
+            statement = connectDB.createStatement();
+            statement.executeUpdate(updateNumOfStars);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connectDB != null) {
+                try {
+                    connectDB.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         JsonFileUtil jsonFileUtil = new JsonFileUtil();
         jsonFileUtil.writeProductReview(imageName,numberOfStars,textArea.getText(),User.getUsername(),"");
